c4598c645041186fe94d320bafd3e31fea406439
Ahmer Ijaz <ahmerijaz639@gmail.com>
2026-08-02 12:18:27 +0500

    update

---
+++ week01/day01/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const Myname = "Ahmer Ijaz"
+	fmt.Println("Name : ", Myname)
+
+	age := 25
+	fmt.Println("Age : ", age)
+
+	var city string = "lahore"
+	fmt.Println("City : ", city)
+
+	var isStudent bool = true
+	fmt.Println("Is Student : ", isStudent)
+
+	var salary float64 = 500.5
+	fmt.Println("Salary : ", salary)
+
+}
